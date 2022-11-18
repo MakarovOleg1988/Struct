6478a66b0fce8a758a6f3294e320f732e5fc9cd9
MakarovOleg1988 <makarov.oleg1988@gmail.com>
2022-11-18 12:42:01 +0530

    Добавьте файлы проекта.

---
+++ Struct/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ExampleStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = Convert.ToInt32(Console.ReadLine());
+            int width = Convert.ToInt32(Console.ReadLine());
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            Cuboid cuboid = new Cuboid(length, width, height);
+
+            Console.WriteLine("Volume: " + cuboid.Volume());
+            Console.WriteLine("Perimeter: " + cuboid.Perimeter());
+        }
+    }
+
+    struct Cuboid
+    {
+        public int length;
+        public int width;
+        public int height;
+
+        public Cuboid(int length, int width, int height)
+        {
+            this.length = length;
+            this.width = width;
+            this.height = height;
+        }
+
+
+
+        public int Volume()
+        {
+            return length * width * height;
+        }
+        //завершите данный метод
+        public int Perimeter()
+        {
+            return 4 * (length + width + height);
+        }
+    }
+}
